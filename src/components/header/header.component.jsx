1531368a35688ab45b183b13bd1b1b9b091f6b0b
RonenTGreat <ronenh53@gmail.com>
2023-06-28 20:07:26 +0000

    done with header and nav

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,45 +1,43 @@
 import logo from "../../assets/logo.png";
-import "./header.styles.css"
+import "./header.styles.css";
 
 const Header = () => {
   return (
-    <>
-      <header>
-        <a href="/.">
-          <img src={logo} alt="logo" />
-        </a>
-        <nav>
-          <ul className="nav-list">
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                About us
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                Services
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                Use Cases
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                Pricing
-              </a>
-            </li>
-            <li className="nav-item">
-              <a href="/#" className="nav-link">
-                Blog
-              </a>
-            </li>
-            <a href="/#">Request a quote</a>
-          </ul>
-        </nav>
-      </header>
-    </>
+    <header>
+      <a href="/.">
+        <img src={logo} className="logo" alt="logo" />
+      </a>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li className="nav-item">
+            <a href="/#" className="nav-link">
+              About us
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/#" className="nav-link">
+              Services
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/#" className="nav-link">
+              Use Cases
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/#" className="nav-link">
+              Pricing
+            </a>
+          </li>
+          <li className="nav-item">
+            <a href="/#" className="nav-link">
+              Blog
+            </a>
+          </li>
+          <a href="/#" className="btn-link">Request a quote</a>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
