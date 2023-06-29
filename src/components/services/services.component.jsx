685e10d780bbdf9e3be1a2490d9c43c30ab83b13
RonenTGreat <ronenh53@gmail.com>
2023-06-29 13:56:28 +0000

    added cards will customize it later

--- src/components/services/services.component.jsx
+++ src/components/services/services.component.jsx
@@ -1,4 +1,6 @@
 import "./servives.styles.css";
+import icon from "../../assets/go-icon.jpg"
+import one from "../../assets/search.png"
 
 const Services = () => {
   return (
@@ -10,6 +12,56 @@ const Services = () => {
           businesses grow and succeed online. These services include:
         </p>
       </article>
+      <div className="services__cards">
+        <article className="card">
+            <div className="card__details">
+                <h3>Search engine</h3>
+                <h3>optimization</h3>
+                <a href="/#"><img src={icon} alt="" /> Learn more</a>
+            </div>
+            <img src={one} alt="" />
+        </article>
+        <article className="card">
+            <div className="card__details">
+                <h3>Search engine</h3>
+                <h3>optimization</h3>
+                <a href="/#"><img src={icon} alt="" /> Learn more</a>
+            </div>
+            <img src={one} alt="" />
+        </article>
+        <article className="card">
+            <div className="card__details">
+                <h3>Search engine</h3>
+                <h3>optimization</h3>
+                <a href="/#"><img src={icon} alt="" /> Learn more</a>
+            </div>
+            <img src={one} alt="" />
+        </article>
+        <article className="card">
+            <div className="card__details">
+                <h3>Search engine</h3>
+                <h3>optimization</h3>
+                <a href="/#"><img src={icon} alt="" /> Learn more</a>
+            </div>
+            <img src={one} alt="" />
+        </article>
+        <article className="card">
+            <div className="card__details">
+                <h3>Search engine</h3>
+                <h3>optimization</h3>
+                <a href="/#"><img src={icon} alt="" /> Learn more</a>
+            </div>
+            <img src={one} alt="" />
+        </article>
+        <article className="card">
+            <div className="card__details">
+                <h3>Search engine</h3>
+                <h3>optimization</h3>
+                <a href="/#"><img src={icon} alt="" /> Learn more</a>
+            </div>
+            <img src={one} alt="" />
+        </article>
+      </div>
     </section>
   );
 };
