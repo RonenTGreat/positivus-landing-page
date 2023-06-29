8a3a91f4934edce1cdbbddf7ba1b91c6db31fff8
RonenTGreat <ronenh53@gmail.com>
2023-06-29 13:22:08 +0000

    created Services component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import "./App.css";
 import Header from "./components/header/header.component";
 import Hero from "./components/hero/hero.component";
+import Services from "./components/services/services.component";
 
 function App() {
   return (
     <>
       <Header />
       <Hero />
+      <Services />
     </>
   );
 }
---
+++ src/components/services/services.component.jsx
@@ -0,0 +1,17 @@
+import "./servives.styles.css";
+
+const Services = () => {
+  return (
+    <section className="services-section container">
+      <article className="services__header">
+        <h2>Services</h2>
+        <p>
+          At our digital marketing agency, we offer a range of services to help
+          businesses grow and succeed online. These services include:
+        </p>
+      </article>
+    </section>
+  );
+};
+
+export default Services;
