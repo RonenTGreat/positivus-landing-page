353cbad4554526f48e5cb073a5e6174139a6799e
RonenTGreat <ronenh53@gmail.com>
2023-06-30 22:24:17 +0000

    added with customizing cards

--- src/components/services/services.component.jsx
+++ src/components/services/services.component.jsx
@@ -1,6 +1,12 @@
 import "./servives.styles.css";
-import icon from "../../assets/go-icon.jpg"
+import icon from "../../assets/go-icon.png"
+import icon1 from "../../assets/go-icon-1.png"
 import one from "../../assets/search.png"
+import pay from "../../assets/pay.png"
+import marketing from "../../assets/marketing.png"
+import email from "../../assets/email.png"
+import content from "../../assets/content.png"
+import tracking from "../../assets/tracking.png"
 
 const Services = () => {
   return (
@@ -13,6 +19,8 @@ const Services = () => {
         </p>
       </article>
       <div className="services__cards">
+
+      {/* Card 1 */}
         <article className="card">
             <div className="card__details">
                 <h3>Search engine</h3>
@@ -21,45 +29,55 @@ const Services = () => {
             </div>
             <img src={one} alt="" />
         </article>
-        <article className="card">
+
+        {/* Card 2 */}
+        <article className="card green-bg">
             <div className="card__details">
-                <h3>Search engine</h3>
-                <h3>optimization</h3>
+                <h3 className="white-bg">Pay-per-click</h3>
+                <h3 className="white-bg">advertising</h3>
                 <a href="/#"><img src={icon} alt="" /> Learn more</a>
             </div>
-            <img src={one} alt="" />
+            <img src={pay} alt="" />
         </article>
-        <article className="card">
+
+        {/* Card 3 */}
+        <article className="card black-bg">
             <div className="card__details">
-                <h3>Search engine</h3>
-                <h3>optimization</h3>
-                <a href="/#"><img src={icon} alt="" /> Learn more</a>
+                <h3 className="white-bg">Social Media</h3>
+                <h3 className="white-bg">Marketing</h3>
+                <a href="/#"><img src={icon1} alt="" /> Learn more</a>
             </div>
-            <img src={one} alt="" />
+            <img src={marketing} alt="" />
         </article>
+
+        {/* Card 4 */}
         <article className="card">
             <div className="card__details">
-                <h3>Search engine</h3>
-                <h3>optimization</h3>
+                <h3>Email</h3>
+                <h3>Marketing</h3>
                 <a href="/#"><img src={icon} alt="" /> Learn more</a>
             </div>
-            <img src={one} alt="" />
+            <img src={email} alt="" />
         </article>
-        <article className="card">
+
+        {/* Card 5 */}
+        <article className="card green-bg">
             <div className="card__details">
-                <h3>Search engine</h3>
-                <h3>optimization</h3>
+                <h3 className="white-bg">Content</h3>
+                <h3 className="white-bg">Creation</h3>
                 <a href="/#"><img src={icon} alt="" /> Learn more</a>
             </div>
-            <img src={one} alt="" />
+            <img src={content} alt="" />
         </article>
-        <article className="card">
+
+        {/* Card 6 */}
+        <article className="card black-bg">
             <div className="card__details">
-                <h3>Search engine</h3>
-                <h3>optimization</h3>
-                <a href="/#"><img src={icon} alt="" /> Learn more</a>
+                <h3>Analytics and </h3>
+                <h3>Tracking</h3>
+                <a href="/#"><img src={icon1} alt="" /> Learn more</a>
             </div>
-            <img src={one} alt="" />
+            <img src={tracking} alt="" />
         </article>
       </div>
     </section>
