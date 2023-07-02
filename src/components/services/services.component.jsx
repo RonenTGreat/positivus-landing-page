fc35bffc4695586a3ba5891a3d79c4e58bb6e781
RonenTGreat <ronenh53@gmail.com>
2023-07-02 11:01:40 +0000

    done with service card

--- src/components/services/services.component.jsx
+++ src/components/services/services.component.jsx
@@ -13,7 +13,7 @@ const Services = () => {
   return (
     <section className="services-section container">
       <article className="services__header">
-        <h2>Services</h2>
+        <h2 className="sub-heading">Services</h2>
         <p>
           At our digital marketing agency, we offer a range of services to help
           businesses grow and succeed online. These services include:
